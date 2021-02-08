03674f2cef33200b9ffdab5035d5943c0640df26
maxgonpe <maxgonpe@gmail.com>
2021-02-08 15:12:38 -0300

    change configurations for travis-cl, eigth time now working yet with flake8 fixed strings in two lines

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,24 +84,24 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
-# AUTH_PASSWORD_VALIDATORS = [
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimila
-# rityValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.MinimumLength
-# Validator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.Common
-# PasswordValidator',
-#    },
-#    {
-#        'NAME': 'django.contrib.auth.password_validation.N
-# umericPasswordValidator',
-#    },
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator',
+    },
+]
 
 
 # Internationalization
